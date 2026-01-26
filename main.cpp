f922db73715aa368c03e48eb5925a5a9f5bd84ca
SatoriSh <satorizoom51@gmail.com>
2026-01-26 17:01:40 +0100

    upd main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
-#include "raylib.h"
 #include "src/game.h"
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
 
-int main()
-{
-    InitAudioDevice();
+Game* game;
 
-    Game game;
+// функция которую браузер будет вызывать каждый кадр
+void UpdateDrawFrame() {
+    game->process(); 
+}
 
-    game.process();
-    
-    CloseAudioDevice();
-    
+int main() {
+    game = new Game();
+
+#ifdef __EMSCRIPTEN__
+    // 0 - использовать FPS браузера, 1 = симулировать бесконечный цикл
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    while (!WindowShouldClose()) {
+        UpdateDrawFrame();
+    }
+#endif
+
+    delete game;
     return 0;
 }
