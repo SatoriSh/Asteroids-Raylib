d2dc8e8592c4f477d07c99b76c38b35fc9ca98ce
SatoriSh <satorizoom51@gmail.com>
2026-01-28 13:02:11 +0100

    added support for WASM in game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -3,102 +3,66 @@
 Game::Game()
 {
     initWindow();
-
     bulletTexture = LoadTexture("src/sprites/bullet.png");
     asteroidTexture = LoadTexture("src/sprites/asteroid.png");
-
     explosionSound = LoadSound("src/sounds/explosion.wav");
     SetSoundVolume(explosionSound, 0.25f);
-
     playerInit();
     entities[0]->isAlive = false;
-
     score = 0;
     currentLevel = 0;
-
-    asteroidSpawnConfig = {
-        {7.5f, 3},
-        {6.5f, 5},
-        {5.0f, 6}
-    };
-
+    asteroidSpawnConfig = {{7.5f, 3}, {6.5f, 5}, {5.0f, 6}};
     asteroidSpawnTimer.timeoutTime = asteroidSpawnConfig[currentLevel].timeToSpawn;
     asteroidSpawnTimer.leftTime = asteroidSpawnTimer.timeoutTime;
-    
+
     generateStarsOnce();
     spawnAsteroids();
 }
 
 void Game::process()
 {
-    while (!WindowShouldClose())
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawTextureRec(starsTexture.texture,
+                   Rectangle{0, 0, (float)starsTexture.texture.width, -(float)starsTexture.texture.height},
+                   Vector2{0, 0}, WHITE);
+    if (checkIfPlayerDie())
     {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        DrawTextureRec(
-            starsTexture.texture,
-            Rectangle{0, 0, (float)starsTexture.texture.width, -(float)starsTexture.texture.height},
-            Vector2{0, 0},
-            WHITE
-        );
-
-        if (checkIfPlayerDie())
-            continue;
-
-        asteroidSpawnTimer.update();
-        
-        checkCollisions();
-
-        if (asteroidSpawnTimer.timeout())
-        {
-            asteroidSpawnTimer.reset();
-            spawnAsteroids();
-        }
-
-        for (std::unique_ptr<GameObject> &entity : entities)
-        {
-            entity->process();
-            checkBounds(entity.get());
-        }
-
-        if (!entitiesToAdd.empty())
-        {
-            for (std::unique_ptr<GameObject> &entity : entitiesToAdd)
-            {
-                entities.push_back(std::move(entity));
-            }
-            entitiesToAdd.clear();
-        }
-
-        entities.erase(
-                std::remove_if(
-                    entities.begin() + 1,
-                    entities.end(),
-                    [](const std::unique_ptr<GameObject>& entity) 
-                    { 
-                        return !entity->isAlive;
-                    }
-                ),
-                entities.end()
-            );
-
-        DrawText(TextFormat("%d", score), screenWidth / 2 - 100, 45, 60, WHITE);
-        DrawFPS(10, 10);
-        EndDrawing();
+        return;
     }
+    asteroidSpawnTimer.update();
 
-    CloseWindow();
+    checkCollisions();
+    if (asteroidSpawnTimer.timeout())
+    {
+        asteroidSpawnTimer.reset();
+        spawnAsteroids();
+    }
+    for (std::unique_ptr<GameObject> &entity : entities)
+    {
+        entity->process();
+        checkBounds(entity.get());
+    }
+    if (!entitiesToAdd.empty())
+    {
+        for (std::unique_ptr<GameObject> &entity : entitiesToAdd)
+        {
+            entities.push_back(std::move(entity));
+        }
+        entitiesToAdd.clear();
+    }
+    entities.erase(std::remove_if(entities.begin() + 1, entities.end(),
+                                  [](const std::unique_ptr<GameObject> &entity) { return !entity->isAlive; }),
+                   entities.end());
+    DrawText(TextFormat("%d", score), screenWidth / 2 - 100, 45, 60, WHITE);
+    DrawFPS(10, 10);
+    EndDrawing();
 }
 
 void Game::playerShoot()
 {
-    entitiesToAdd.push_back(std::make_unique<Bullet>(
-        entities[0]->getPos().x,
-        entities[0]->getPos().y, 
-        entities[0]->getRotation(), 
-        bulletTexture)
-    );
+    entitiesToAdd.push_back(std::make_unique<Bullet>(entities[0]->getPos().x, entities[0]->getPos().y,
+                                                     entities[0]->getRotation(), bulletTexture));
 }
 
 void Game::spawnAsteroids()
@@ -107,13 +71,8 @@ void Game::spawnAsteroids()
     {
         float x = GetRandomValue(0, screenWidth);
         float y = GetRandomValue(1, 2) == 1 ? -spawnOffset : screenHeight + spawnOffset;
-
-        entities.push_back(std::make_unique<Asteroid>(
-            x, y, 
-            GetRandomValue(1, 3), 
-            Vector2{entities[0]->getPos()}, 
-            asteroidTexture)
-        );
+        entities.push_back(
+            std::make_unique<Asteroid>(x, y, GetRandomValue(1, 3), Vector2{entities[0]->getPos()}, asteroidTexture));
     }
 }
 
@@ -127,21 +86,16 @@ void Game::checkCollisions()
         {
             if (entity1->gameObjectType == entity2->gameObjectType)
                 continue;
-
-
             if (entity2->gameObjectType == GameObject::GameObjectTypeEnum::ASTEROID)
             {
                 if (CheckCollisionRecs(entity1->getRec(), entity2->getRec()))
                 {
                     PlaySound(explosionSound);
-
                     Asteroid *asteroid = dynamic_cast<Asteroid *>(entity2.get());
-
                     if (asteroid->asteroidLvl > 1)
                         splitAsteroid(asteroid->asteroidLvl, asteroid->getPos());
-
                     updateScore(asteroid->asteroidLvl);
-                    
+
                     entity2->isAlive = false;
                     entity1->isAlive = false;
                 }
@@ -153,17 +107,12 @@ void Game::checkCollisions()
 void Game::splitAsteroid(int asteroidLvl, Vector2 position)
 {
     int asteroidsCount = asteroidLvl == 2 ? 3 : 2;
-
     for (int i = 0; i < asteroidsCount; i++)
     {
-        Vector2 direction = {GetRandomValue(GetScreenWidth() / 2, GetScreenWidth() / 4), GetRandomValue(GetScreenWidth() / 2, GetScreenHeight() / 4)};
-
-        entitiesToAdd.push_back(std::make_unique<Asteroid>(
-            position.x, position.y, 
-            asteroidLvl - 1, 
-            direction, 
-            asteroidTexture)
-        );
+        Vector2 direction = {GetRandomValue(GetScreenWidth() / 2, GetScreenWidth() / 4),
+                             GetRandomValue(GetScreenWidth() / 2, GetScreenHeight() / 4)};
+        entitiesToAdd.push_back(
+            std::make_unique<Asteroid>(position.x, position.y, asteroidLvl - 1, direction, asteroidTexture));
     }
 }
 
@@ -173,7 +122,6 @@ void Game::checkBounds(GameObject *entity)
         entity->setX(0);
     if (entity->getPos().x < -spawnOffset)
         entity->setX(screenWidth);
-
     if (entity->getPos().y > screenHeight + spawnOffset)
         entity->setY(0);
     if (entity->getPos().y < -spawnOffset)
@@ -183,25 +131,19 @@ void Game::checkBounds(GameObject *entity)
 void Game::generateStarsOnce()
 {
     starsTexture = LoadRenderTexture(screenWidth, screenHeight);
-
     BeginTextureMode(starsTexture);
-
     for (int i = 0; i < starsCount; i++)
     {
         DrawCircle(GetRandomValue(1, screenWidth), GetRandomValue(1, screenHeight), 1.0f, VIOLET);
     }
-
     EndTextureMode();
 }
 
 void Game::playerInit()
 {
-    auto player = std::make_unique<Player>(
-        screenWidth / 2, screenHeight / 2); // сначала создаю player здесь чтобы удобно подключиться к его событиям
-
+    auto player = std::make_unique<Player>(screenWidth / 2, screenHeight / 2);
     player->onShoot = [this]() { playerShoot(); };
-
-    entities.push_back(std::move(player)); // переменная player теперь пустая, объект ушел в вектор
+    entities.push_back(std::move(player));
 }
 
 void Game::updateScore(int destroyedAsteroidLvl)
@@ -212,7 +154,6 @@ void Game::updateScore(int destroyedAsteroidLvl)
         score += 50;
     else if (destroyedAsteroidLvl == 3)
         score += 15;
-
     if (score >= 5000)
     {
         currentLevel = 1;
@@ -229,11 +170,6 @@ bool Game::checkIfPlayerDie()
 {
     if (!entities[0]->isAlive)
     {
-        DrawText("Press enter to start", screenWidth / 2 - 250, screenHeight / 2, 40, YELLOW);
-
-        if (score > 0)
-            DrawText(TextFormat("%d", score), screenWidth / 2 - 100, 45, 60, WHITE);
-
         if (IsKeyPressed(KEY_ENTER))
         {
             entities.clear();
@@ -243,7 +179,9 @@ bool Game::checkIfPlayerDie()
             score = 0;
             return false;
         }
-
+        DrawText("Press enter to start", screenWidth / 2 - 250, screenHeight / 2, 40, YELLOW);
+        if (score > 0)
+            DrawText(TextFormat("%d", score), screenWidth / 2 - 100, 45, 60, WHITE);
         EndDrawing();
         return true;
     }
@@ -253,12 +191,9 @@ bool Game::checkIfPlayerDie()
 void Game::initWindow()
 {
     SetConfigFlags(FLAG_WINDOW_UNDECORATED);
-
     InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "Asteroids");
     SetTargetFPS(FPS);
-
     MaximizeWindow();
-
     screenWidth = GetScreenWidth();
     screenHeight = GetScreenHeight();
 }
